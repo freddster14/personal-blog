7ef285904064bdb9ce65302d58162c2c0772e032
freddster14 <freddycarrillo951@gmail.com>
2025-12-29 21:50:15 -0600

    Add user routes and controller methods

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -1,7 +1,52 @@
+import { prisma } from "../prisma/client.js";
+
 export const home = (req, res) => {
   res.send('home')
 }
 
 export const about = (req, res) => {
   res.send('about')
+}
+
+export const signup = async (req, res, next) => {
+  res.send('signup')
+}
+
+export const login = async (req, res) => {
+  res.send("login")
+}
+
+export const loginUser = async (req, res, next) => {
+  const { email , password } = req.body;
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: 'idk'
+      }
+    })
+  } catch (error) {
+    next(error);
+  }
+}
+export const logout = async (req, res, next) => {
+
+  res.send('logout')
+}
+
+
+
+export const create = async (req, res, next) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      }
+    });
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
 }
--- server/routes/home.js
+++ server/routes/home.js
@@ -3,6 +3,12 @@ import * as controller from '../controllers/home.js';
 const home = Router();
 
 home.get('/', controller.home);
-home.get('/about', controller.about)
+home.get('/about', controller.about);
+home.get('/sign-up', controller.signup);
+home.get('/login', controller.login);
+
+home.post('/sign-up', controller.create);
+home.post('/login', controller.loginUser);
+home.post('/logout', controller.logout);
 
 export default home;
