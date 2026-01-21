775482b983dfc634fe2f2fffe8b99cc01ac0a8b3
freddster14 <freddycarrillo951@gmail.com>
2026-01-21 08:53:51 -0600

    fix cors issue

--- server/app.js
+++ server/app.js
@@ -6,8 +6,14 @@ import commentRoutes from './routes/comment.js';
 
 const PORT = 3000;
 const app = express();
+const corsOptions = {
+  origin: 'https://personal-blog-sepia-iota.vercel.app/',
+  methods: 'GET, POST, PUT, DELETE',
+  allowedHeaders: 'Content-Type, Authorization',
+}
 
-app.use(cors())
+
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
